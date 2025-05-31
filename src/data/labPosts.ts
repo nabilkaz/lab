bcd0b5ec92dc86441448078f80ef7357bf378afc
Nabil Kazerouni <nabilkaz@gmail.com>
2025-05-31 16:58:08 -0700

    CREATE mock labPosts data

---
+++ src/data/labPosts.ts
@@ -0,0 +1,34 @@
+import { LabPostPreviewProps } from "@/components/LabPostPreview/LabPostPreview";
+
+export const mockLabPosts: LabPostPreviewProps[] = [
+  {
+    title: "My First Post",
+    tag: "Tag",
+    publishedDate: "2025-01-01",
+    excerpt: "This is my first post",
+  },
+  {
+    title: "My Second Post",
+    tag: "Tag",
+    publishedDate: "2025-01-02",
+    excerpt: "This is my second post",
+  },
+  {
+    title: "My Third Post",
+    tag: "Tag",
+    publishedDate: "2025-01-03",
+    excerpt: "This is my third post",
+  },
+  {
+    title: "My Fourth Post",
+    tag: "Tag",
+    publishedDate: "2025-01-04",
+    excerpt: "This is my fourth post",
+  },
+  {
+    title: "My Fifth Post",
+    tag: "Tag",
+    publishedDate: "2025-01-05",
+    excerpt: "This is my fifth post",
+  },
+];
